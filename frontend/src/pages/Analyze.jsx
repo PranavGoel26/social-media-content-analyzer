e53bd452d99964be3404ed0ff94276e03a80f54c
PranavGoel26 <pranavgoell2023@gmail.com>
2026-02-19 15:49:17 +0530

    ui change commit last

--- frontend/src/pages/Analyze.jsx
+++ frontend/src/pages/Analyze.jsx
@@ -61,83 +61,7 @@ const analyzeFile = async () => {
 
 
 
-//   return (
-//     <div className="max-w-4xl mx-auto">
-
-//       <h2 className="text-3xl font-bold text-slate-800 mb-6 text-center">
-//         Analyze Your Post
-//       </h2>
-
-//       <div className="bg-white p-8 rounded-xl shadow-sm border border-slate-200">
-
-//         <textarea
-//           className="w-full p-4 rounded-lg border border-slate-300 focus:ring-2 focus:ring-blue-500 focus:outline-none mb-6"
-//           rows="6"
-//           placeholder="Paste your social media post here..."
-//           value={text}
-//           onChange={(e) => setText(e.target.value)}
-//         />
-
-//         <div className="mt-6 border-t pt-6">
-
-//   <input
-//     type="file"
-//     accept=".pdf,image/*"
-//     onChange={(e) => setFile(e.target.files[0])}
-//     className="mb-4"
-//   />
-
-//   <button
-//     onClick={analyzeFile}
-//     disabled={loading}
-//     className="bg-purple-600 text-white px-6 py-2 rounded-lg hover:bg-purple-700 transition disabled:opacity-50"
-//   >
-//     {loading ? "Analyzing File..." : "Analyze File"}
-//   </button>
-
-// </div>
-
-
-//         <div className="text-center">
-//           <button
-//               onClick={analyzePost}
-//               disabled={loading}
-//               className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition disabled:opacity-50 flex items-center gap-2"
-//             >
-//               {loading && (
-//                 <span className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin"></span>
-//               )}
-//               {loading ? "Analyzing..." : "Analyze Post"}
-//         </button>
-//         {loading && (
-//           <p className="mt-4 text-blue-600 font-medium">
-//             Please wait, your file is getting analyzed....
-//           </p>
-//         )}
-
-
-//         </div>
-
-//       </div>
-
-//       {result && (
-//         <div className="mt-8 bg-white p-8 rounded-xl shadow-sm border border-slate-200">
-//           <h3 className="text-xl font-semibold mb-4 text-slate-800">
-//             AI Response
-//           </h3>
-
-//           <pre className="whitespace-pre-wrap text-slate-700 text-sm leading-relaxed">
-//             {result}
-//           </pre>
-//         </div>
-//       )}
-
-//     </div>
-//   )
-// }
-
-
-return (
+  return (
   <div className="max-w-4xl mx-auto">
 
     <h2 className="text-3xl font-bold text-slate-800 mb-8 text-center">
@@ -218,6 +142,6 @@ return (
   </div>
 )
 
-
+}
 
 export default Analyze
